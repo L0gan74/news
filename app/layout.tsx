a53c39ad38526d20dfedaa2588ea33f70e00e517
31011972Qw <ggtrfr@bk.ru>
2023-07-15 16:37:58 +0300

    commit 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,9 @@
 import Header from "@/components/header/header";
 import "./globals.css";
 import Footer from "@/components/footer/footer";
-import { Metadata } from "next";
 import { Providers } from "@/store/provider";
 
 import { NextAuthSessionProvider } from "./sessionProvuder";
-
-export const metadata: Metadata = {
-  title: "L0gan News",
-  description: "description description descriptiondescription v",
-};
 
 export default function RootLayout({
   children,
